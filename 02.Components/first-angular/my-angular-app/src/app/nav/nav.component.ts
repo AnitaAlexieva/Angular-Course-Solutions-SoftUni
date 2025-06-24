88ad8aae0bb2d03660086e930bc7f711e0758d1a
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-06-24 23:03:45 +0300

    My first angular component

---
+++ 02.Components/first-angular/my-angular-app/src/app/nav/nav.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-nav',
+    template: '<div id="nav-wrapper">Hello navigation</div>',
+    styles:['#nav-wrapper:{color:pink}'],
+})
+export class NavComponent{}
