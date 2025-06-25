6d59ff3f21e8587e8107898b72aa898829c3da6c
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-06-25 12:13:50 +0300

    Added *ngFor loop to display a list of users with name and age

--- 02.Components/first-angular/my-angular-app/src/app/user-list/user-list.component.ts
+++ 02.Components/first-angular/my-angular-app/src/app/user-list/user-list.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 
+type User = {
+  name:string;
+  age:number;
+};
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent {
+  users = [
+    {name:'Pesho', age:21},
+    {name:'Raq', age:31},
+    {name:'Petq', age:29},
+    {name:'Katq', age:41},
+    {name:'Gosho', age:53},
+  ] as User[];
+  //User-list е колекция на User
 }
