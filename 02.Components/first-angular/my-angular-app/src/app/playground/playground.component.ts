cd7045119ccc8d7b319202f5255ada485aa50ce1
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-06-26 23:29:10 +0300

    Add in playground component  input binding for color

--- 02.Components/first-angular/my-angular-app/src/app/playground/playground.component.ts
+++ 02.Components/first-angular/my-angular-app/src/app/playground/playground.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-playground',
@@ -7,8 +7,11 @@ import { Component } from '@angular/core';
 })
 export class PlaygroundComponent {
 
+  @Input('color') colorValue = 'white';
+
     handleInput(username:string){
       console.log(username);
+      console.log(this.colorValue)
     }
 
 }
