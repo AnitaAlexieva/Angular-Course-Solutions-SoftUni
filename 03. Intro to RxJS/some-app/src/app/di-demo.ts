705af302bbf4463ff4eb118bcf90d7b96bf71279
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-07-01 23:53:36 +0300

    Implement basic OOP classes with constructor injection

--- 03. Intro to RxJS/some-app/src/app/di-demo.ts
+++ 03. Intro to RxJS/some-app/src/app/di-demo.ts
@@ -1,24 +1,3 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { UserItemComponent } from './user-item/user-item.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    UserListComponent,
-    UserItemComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
-
 class Wallet{
   balance:number;
 
