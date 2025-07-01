7e428b4acc3e45dad5b69b8d192876af6e9d6cee
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-07-01 23:34:29 +0300

    Interface segregation principle

--- 03. Intro to RxJS/some-app/src/app/types/user.ts
+++ 03. Intro to RxJS/some-app/src/app/types/user.ts
@@ -2,3 +2,36 @@ export interface User{
     name:string;
     age:number;
 }
+
+interface Geo{
+        "lat": string,
+        "lng": string
+      }
+interface Address{
+      "street": string,
+      "suite": string,
+      "city": string,
+      "zipcode": string,
+      "geo": Geo
+    }
+
+interface Company{
+      "name": string,
+      "catchPhrase":string,
+      "bs": string
+    }
+
+export interface User2{
+  
+    "id": number,
+    "name": string,
+    "username": string,
+    "email": string,
+    "phone": string,
+
+}
+export interface jsonPlaceholderUser extends User2{
+    "address": Address,
+    "phone": string,
+    "company": Company
+  }
