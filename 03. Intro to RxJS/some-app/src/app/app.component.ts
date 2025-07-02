b7559a068d2244404fa283eb31c3397e5cf90048
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-07-02 19:35:58 +0300

    Inject the userService functionality

--- 03. Intro to RxJS/some-app/src/app/app.component.ts
+++ 03. Intro to RxJS/some-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { User } from './types/user';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,7 @@ import { User } from './types/user';
 export class AppComponent {
   title = 'My playground';;
 
+  constructor(public userService: UserService){
+    
+  }
 }
