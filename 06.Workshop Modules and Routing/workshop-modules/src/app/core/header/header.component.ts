d9427e3d90496b70df1311776d834469f46095ed
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-07-16 10:33:18 +0300

    Add logout and login functionality in nav

--- 06.Workshop Modules and Routing/workshop-modules/src/app/core/header/header.component.ts
+++ 06.Workshop Modules and Routing/workshop-modules/src/app/core/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {  Router } from '@angular/router';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  isLogged:boolean = false;
+  
+  constructor(
+    private userService:UserService,
+    private router:Router
+  ){}
+
+  get isLogged():boolean{
+    return this.userService.isLogged
+  }
+
+  get username():string{
+    return this.userService.user?.username || ''
+  }
+
+  logout(){
+    this.userService.logout()
+    this.router.navigate(['/home'])
+  }
 }
