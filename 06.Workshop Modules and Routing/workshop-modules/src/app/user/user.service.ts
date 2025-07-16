6c17a500c263507078914008e3d2a81e0cd550e7
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-07-16 10:18:48 +0300

    Add login functionality

--- 06.Workshop Modules and Routing/workshop-modules/src/app/user/user.service.ts
+++ 06.Workshop Modules and Routing/workshop-modules/src/app/user/user.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { UserForAuth } from '../types/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  
+  
+  user:UserForAuth | undefined;
+  USER_KEY="[user]";
+  
+  get isLogged():boolean{
+    return !!this.user
+  }
+  
+  constructor() { 
+   try{
+     const lsUser = localStorage.getItem(this.USER_KEY) || '';
+    this.user=JSON.parse(lsUser)
+   }catch(error){
+    this.user=undefined
+   }
+  }
 
-  constructor() { }
+  login(){
+      this.user ={
+        username:'Petko',
+        email:'petko@abv.bg',
+        phone:'123-123-321',
+        password:'petko123'
+      }
+
+      localStorage.setItem(this.USER_KEY, JSON.stringify(this.user))
+  }
+
+  logout(){ 
+
+    this.user=undefined;
+    localStorage.removeItem(this.USER_KEY)
+
+  }
 }
