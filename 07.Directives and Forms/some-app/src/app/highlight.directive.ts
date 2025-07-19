5040a7cb91f3050896d9fe76fb9ddc6e3bb2e285
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-07-19 23:28:50 +0300

    Add hover effect in HighlightDirective using Renderer2

--- 07.Directives and Forms/some-app/src/app/highlight.directive.ts
+++ 07.Directives and Forms/some-app/src/app/highlight.directive.ts
@@ -10,11 +10,26 @@ export class HighlightDirective implements OnInit {
       console.log(this.elRef)
 
       // Bad practise
-      this.elRef.nativeElement.style.background = 'orange'
+      // this.elRef.nativeElement.style.background = 'orange'
 
       //Good practise
-      this.renderer.setStyle(this.elRef.nativeElement, 'background', 'purple')
+      // this.renderer.setStyle(this.elRef.nativeElement, 'background', 'purple')
 
+      this.renderer.listen(
+        this.elRef.nativeElement, 'mouseenter', this.mouseEnterHandler.bind(this)
+      )
+
+      this.renderer.listen(
+        this.elRef.nativeElement, 'mouseleave', this.mouseLeaveHandler.bind(this)
+      )
   }
 
+  mouseEnterHandler(e: MouseEvent): void{
+    console.log(e)
+    this.renderer.setStyle(this.elRef.nativeElement, 'background', 'pink')
+  }
+
+  mouseLeaveHandler(e:MouseEvent):void{
+    this.renderer.setStyle(this.elRef.nativeElement, 'background', 'initial')
+  }
 }
