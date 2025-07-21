3002cf27db59e25488dd2f8dfdad49c92956bc20
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-07-21 18:35:31 +0300

    Add validation to login form using ViewChild and NgForm

--- 07.Directives and Forms/some-app/src/app/login/login.component.ts
+++ 07.Directives and Forms/some-app/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,16 +7,30 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  forSubmitHandler(form:NgForm){
+
+  @ViewChild('loginForm') form:NgForm | undefined;
+
+  forSubmitHandler(){
+
+    if(!this.form){
+      return;
+    }
     
-    console.log(form.value)
+    console.log('Form invalid: ', this.form.invalid)
+    console.log(this.form.value)
+
+    if(this.form?.invalid){
+      console.log('Form is invalid')
+        return;
+      
+    }
 
     //form.value => ngModel on input
-    const {email, password} = form?.value
+    const {email, password} = this.form?.value
 
     //2 ways of reseting the data
     //form.reset()
-    form.setValue({email:'', password:''})
+    this.form.setValue({email:'', password:''})
 
   }
 }
