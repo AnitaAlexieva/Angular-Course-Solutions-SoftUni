53b0330b4beca5ba90b899887c64dffcc577e1c3
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-07-31 07:40:28 +0300

    Create email validation in register form

--- 08.Workshop Forms/workshop-modules/src/app/sharedd/validators/email-validator.ts
+++ 08.Workshop Forms/workshop-modules/src/app/sharedd/validators/email-validator.ts
@@ -2,7 +2,7 @@ import { ValidatorFn } from "@angular/forms";
  
  export function emailValidator(domain:string[]):ValidatorFn{
     const domainString = domain.join('|')
-    const regExp = new RegExp(`[A-Za-z0-9]+@gmail\.(${domainString})`)
+const regExp = new RegExp(`^[^@\\s]+@[^@\\s]+\\.(${domainString})$`, 'i');
     return (control) =>{
       console.log('controlValue: ', control.value === '' || regExp.test(control.value))
       const isEmailValid = regExp.test(control.value)
